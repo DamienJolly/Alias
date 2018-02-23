8d0056f323326176a91b1c3c9bc4ffa62d399ab4
Damien Jolly <DamienJolly@hotmail.com>
2018-02-23 16:48:47 +0000

    Add project files.

---
+++ Alias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
